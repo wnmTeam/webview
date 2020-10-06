1da272f945fc37af8c9ba2afd5f90e6e38350083
OmarAlkad <omar.mhd.drive@gmail.com>
2020-10-06 22:29:56 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home', url: 'https://www.shmran.net/inf/'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title, this.url});
+
+  final String title;
+  final String url;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  WebViewController _controller;
+
+  final Completer<WebViewController> _controllerCompleter =
+  Completer<WebViewController>();
+  //Make sure this function return Future<bool> otherwise you will get an error
+  Future<bool> _onWillPop(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SafeArea(
+            child: WebView(
+              key: UniqueKey(),
+              onWebViewCreated: (WebViewController webViewController) {
+                _controllerCompleter.future.then((value) => _controller = value);
+                _controllerCompleter.complete(webViewController);
+              },
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: widget.url,
+            )),
+      ),
+    );
+  }
+}
